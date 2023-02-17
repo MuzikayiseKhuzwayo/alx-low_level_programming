04d568eec80ac79643e42a23f32fbae105307f93
Muzikayise Khuzwayo <khuzwayomuzikayise@gmail.com>
2023-02-17 14:18:47 -0800

    done 101; comment: it failed me because betty linter warnings pegged my main file as having more than 40 lines. But it is impossible to not have more than 40 lines in main because of betty line skipping and brace rules. hayi.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 /**
  * main - Entry Point.
- * section header: A C program that outputs every combination of three nums
+ * section header: A C program that outputs every combination of two nums
  * in base 10 and outputs them to stdout.
  * Return: returns 0 (Successful)
  */
 int main(void)
 {
 	/*
-	 * use nested while loops to loop through 10 by 10 by 10 times while
-	 * changing the second loops condiotioneverytime appropriately so as
-	 * not to allow duplocates and matching combinations
-	 * to output formatted numbers to stdout stream.
-	 * return 0 after that task has been accomplished
+	 * use nested while loops to loop through 10 by 10 times while
 	 */
 	int i = 0;
 
@@ -20,33 +16,19 @@ int main(void)
 
 	int k = 0;
 
-	while (i <= 9 - 1 - 1)
+	while (i < 9 - 1)
 	{
-		int tmp = i + 1;
+		j = i + 1;
 
-		int tmp2 = tmp;
-
-		j = 0;
-
-		while (j <= 9 - tmp - 1)
+		while (j < 9)
 		{
-			int tmp3 = tmp + 1;
+			k = j + 1;
 
-			int tmp4 = tmp3;
-
-			k = 0;
-
-			int tmp5 = j + 1;
-
-			while (k <= 9 - tmp5 - 1)
+			while (k < 10)
 			{
-				if (tmp4 > 9)
-				{
-					break;
-				}
 				putchar(i + '0');
-				putchar(tmp2 + '0');
-				putchar(tmp4 + '0');
+				putchar(j + '0');
+				putchar(k + '0');
 				if (i < 7)
 				{
 					putchar(',');
@@ -56,10 +38,8 @@ int main(void)
 				{
 					putchar('\n');
 				}
-				tmp4++;
 				k++;
 			}
-			tmp2++;
 			j++;
 		}
 		i++;
