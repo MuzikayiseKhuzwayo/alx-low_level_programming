961d73f7d0e57cf391eaec03af7156db94b1bea3
Muzikayise Khuzwayo <khuzwayomuzikayise@gmail.com>
2023-02-27 13:12:40 -0800

    done: atoi; check: 2

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,9 @@
 
 int _atoi(char *s)
 {
-	int len, i, num, neg, numchk;
+	int len, i, neg, numchk;
+
+	unsigned int num;
 
 	len = 0;
 
@@ -26,37 +28,24 @@ int _atoi(char *s)
 		len++;
 	}
 	for (i = 0; i < len; i++)
-	{
 		s--;
-	}
-
 	if (len == 0)
-	{
-		return (0);
-	}
+		num = 0;
 	else
 	{
-		/* keep mulitplying by ten as long as there are digits*/
-
 		for (i = 0; i < len; i++)
 		{
 			if (s[i] == '-')
-                	{
-                        	neg *= -1;
-                	}
+				neg *= -1;
 			else if (s[i] == ' ' || s[i] == '+')
-                        {
-                                continue;
-                        }
+				continue;
 			else if (s[i] >= 48 && s[i] < 58)
 			{
 				num = num * 10 + (int)(s[i] - '0');
 				numchk++;
 			}
 			else if (numchk)
-			{
 				break;
-			}
 		}
 	}
 	num *= neg;
