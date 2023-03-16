ffe1e81ecd9c1032a5d99fff97b617a7c98607d4
Muzikayise Khuzwayo <khuzwayomuzikayise@gmail.com>
2023-03-16 07:58:35 -0700

    done: all; fixed: 100(freeing a ptr that was still useful lol)

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,7 +18,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 	{
-		free(ptr);
 		return (ptr);
 	}
 	else if (new_size == 0 && ptr != NULL)
