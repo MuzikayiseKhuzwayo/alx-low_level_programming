658197183061266c92bf5a562ac0aef55e493b71
MuzikayiseKhuzwayo <khuzwayomuzikayise@gmail.com>
2023-03-22 18:56:41 +0200

    fixed: 1(header)

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,11 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stddef.h>
+/*
+ * function pointers: header file for function pointers used in task
+ */
 #include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
